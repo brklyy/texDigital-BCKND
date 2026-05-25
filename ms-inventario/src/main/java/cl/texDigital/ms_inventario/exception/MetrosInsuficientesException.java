4a52394f29e41de143ca4b4ad974882608e34719
brancoaliaga51@gmail.com <brancoaliaga51@gmail.com>
2026-05-25 19:50:14 -0400

    feat(ms-inventario): añadido excepciones con GlobalExceptionHandler y otras extras personalizadas

---
+++ ms-inventario/src/main/java/cl/texDigital/ms_inventario/exception/MetrosInsuficientesException.java
@@ -0,0 +1,8 @@
+package cl.texDigital.ms_inventario.exception;
+
+public class MetrosInsuficientesException extends RuntimeException {
+
+    public MetrosInsuficientesException(String message) {
+        super(message);
+    }
+}
