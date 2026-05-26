c45f3dd410155a2c06948b801c68da81b4a0b3fb
Broskyduoc <brun.carrasco@duocuc.cl>
2026-05-26 01:07:26 -0400

    fix: rehecho estructura base produccion

---
+++ ms-produccion/src/main/java/cl/texDigital/ms_produccion/MsProduccionApplication.java
@@ -0,0 +1,13 @@
+package cl.texDigital.ms_produccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProduccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProduccionApplication.class, args);
+	}
+
+}
