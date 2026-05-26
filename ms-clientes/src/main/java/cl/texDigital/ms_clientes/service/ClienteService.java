6396114f41b351830e32bdbbd2b2dd7ce46fcc57
brancoaliaga51@gmail.com <brancoaliaga51@gmail.com>
2026-05-26 18:05:05 -0400

    fix(ms-clientes): agregar @Transactional a ClienteService

--- ms-clientes/src/main/java/cl/texDigital/ms_clientes/service/ClienteService.java
+++ ms-clientes/src/main/java/cl/texDigital/ms_clientes/service/ClienteService.java
@@ -8,12 +8,14 @@ import cl.texDigital.ms_clientes.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 public class ClienteService {
 
     private final ClienteRepository clienteRepository;
