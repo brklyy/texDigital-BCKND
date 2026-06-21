a8d3b3a032341fc6d1e2bb1cbe526b772207b762
brklyy <brancoaliaga51@gmail.com>
2026-06-21 00:28:03 -0400

    feat: agregar JWT SecurityScheme en Swagger de los 7 microservicios

--- ms-reseñas/src/main/java/cl/texDigital/ms_resenas/config/OpenApiConfig.java
+++ ms-reseñas/src/main/java/cl/texDigital/ms_resenas/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package cl.texDigital.ms_resenas.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,6 +24,12 @@ public class OpenApiConfig {
                         .contact(new Contact()
                                 .name("Equipo texDigital")
                                 .url("https://github.com/brklyy/texDigital-BCKND"))
-                        .license(new License().name("Uso academico - DSY1103")));
+                        .license(new License().name("Uso academico - DSY1103")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
