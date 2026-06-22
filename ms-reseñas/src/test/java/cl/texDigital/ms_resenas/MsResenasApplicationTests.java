57f76bfedf6ddd818f40f6de948c2e53d88756a1
solosiss <To.salasm@duocuc.cl>
2026-06-22 16:30:47 -0400

    fix(ms-resenas): agregar @ActiveProfiles en contextLoads test

--- ms-reseñas/src/test/java/cl/texDigital/ms_resenas/MsResenasApplicationTests.java
+++ ms-reseñas/src/test/java/cl/texDigital/ms_resenas/MsResenasApplicationTests.java
@@ -2,8 +2,10 @@ package cl.texDigital.ms_resenas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class MsResenasApplicationTests {
 
 	@Test
